26007433ecda7ff58b23a1bce1809a4417a247b7
Molly Johnson <johnsmol@os1.engr.oregonstate.edu>
2018-11-18 11:56:46 -0800

    with comments added

--- mypython.py
+++ mypython.py
@@ -3,17 +3,34 @@
 #due: 11/19/18
 #ONID: johnsmol
 
+#importing random and string, and getting a specified number of random
+#lowercase chars adapted from:
+#https://stackoverflow.com/questions/2823316/generate-a-random-letter-in-python
 import random
 import string
 
-stringLength = 10
+#function to get a specified number (y) of lowercase chars and join them into
+#a string. Adapted from:
+#https://stackoverflow.com/questions/2823316/generate-a-random-letter-in-python
 def random_char(y):
 		return ''.join(random.choice(string.ascii_lowercase) for x in range(y))
 
+#string length as specified in instructions
+stringLength = 10
+
+#file opening in current working directory  and writing to a file in python adapted from:
+#https://www.w3schools.com/python/python_file_write.asp and
+#http://www.pitt.edu/~naraehan/python3/file_path_cwd.html
+
+#initialize string var of length 10 with the random chars from the raandom_char function
 file1String = random_char(stringLength)
+#concat a newline onto the string
 file1String = file1String + "\n"
+#use w to write to that file
 file1 = open("file1.txt", "w")
+#write string to the file
 file1.write(file1String)
+#close the file
 file1.close()
 
 file2String = random_char(stringLength)
