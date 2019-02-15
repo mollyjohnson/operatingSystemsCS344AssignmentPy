b2f5cc9739fae14fb7d9b48298d6ec300970f2df
Molly Johnson <johnsmol@os1.engr.oregonstate.edu>
2019-02-15 08:47:45 -0800

    with fixed comments

--- mypython.py
+++ mypython.py
@@ -13,8 +13,8 @@
 import random
 import string
 
-#function to get a specified number (y) of lowercase chars and join them into
-#a string. Adapted from:
+#function to get a specified number (y) of random lowercase chars and join them into
+#a string and return this string. Adapted from:
 #https://stackoverflow.com/questions/2823316/generate-a-random-letter-in-python
 def random_char(y):
 		return ''.join(random.choice(string.ascii_lowercase) for x in range(y))
@@ -22,45 +22,60 @@ def random_char(y):
 #set string length as specified in instructions
 stringLength = 10
 
-#file opening in current working directory  and writing to a file in python adapted from:
+#file creating/opening in current working directory and writing to that file in python adapted from:
 #https://www.w3schools.com/python/python_file_write.asp and
 #http://www.pitt.edu/~naraehan/python3/file_path_cwd.html
 
-#initialize string var of length 10 with the random chars from the raandom_char function
+#initialize string var of length 10 with the random chars from the random_char function
 file1String = random_char(stringLength)
 #concat a newline onto the string
 file1String = file1String + "\n"
-#use w to write to that file
+#open the file (using w b/c are going to create and write to that file)
 file1 = open("file1.txt", "w")
 #write string to the file
 file1.write(file1String)
 #close the file
 file1.close()
 
-#do the same for second string
+#do the same for second file/second string
+#initialize string var of length 10 with the random chars from the random_char function
 file2String = random_char(stringLength)
+#concat a newline onto the string
 file2String = file2String + "\n"
+#open the file (using w b/c are going to create &  write to that file)
 file2 = open("file2.txt", "w")
+#write string to the file
 file2.write(file2String)
+#close the file
 file2.close()
 
-#do the same for third string
+#do the same for third file/ third string
+#initialize string var of length 10 with the random chars from the random_char function
 file3String = random_char(stringLength)
+#concat a newline onto the string
 file3String = file3String + "\n"
+#open the file (using w b/c are going to create and write to that file)
 file3 = open("file3.txt", "w")
+#write string to the file
 file3.write(file3String)
+#close the file
 file3.close()
 
 #printing a string with no newline character added at the end (as would happen with print function normally) adapted from:
 #https://www.w3resource.com/python-exercises/python-basic-exercise-50.php
 
 #print from char 0 to 11, with the end being blank (i.e. no additional  newline or space added, will just print exactly what's in the file)
+#(end is "" so that no additional chars are added after the 0-11 chars are printed).
+
+#print chars 0-11 from file 1 to terminal. 
 for i in range (0,11):
 		print(file1String[i], end="")
 
+#print chars 0-11 from file 2 to terminal. 
 for j in range (0,11):
 		print(file2String[j], end="")
 
+#print chars 0-11 from file 3 to terminal. 
 for k in range (0,11):
 		print(file3String[k], end="")
 
@@ -68,10 +83,16 @@ for k in range (0,11):
 #https://www.geeksforgeeks.org/python-generate-random-numbers-within-a-given-range-and-store-in-a-list/
 
 #get two random integers from 1 to 42 (inclusive) and print on separate lines
+
+#set min and max values (inclusive) for the random ints
 min = 1
 max = 42
+
+#get two random numbers in the specified range
 num1 = random.randint(min, max)
 num2 = random.randint(min, max) 
+
+#print the two random numbers (using just the print function will add a newline)
 print(num1)
 print(num2)
 
